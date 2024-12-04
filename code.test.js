19a1faf9f3d95f4ca9e0bdf643499bdb911b7dde
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-12-04 11:02:34 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,4 +4,4 @@ eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
-assert(bottomUp(10) === 1654885615)
+assert(bottomUp(10) === 1654885615);
