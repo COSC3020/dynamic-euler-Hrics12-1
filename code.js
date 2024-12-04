777e98063c9b9d66dd53271f0c5d70b8c5af440b
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-12-04 09:07:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,24 @@
 function factorial(n) {
-    if(n === 0) return 1;
+    if (n === 0) return 1;
     else return n * factorial(n - 1);
 }
 
 function e(n) {
-    if(n === 0) return 1;
+    if (n === 0) return 1;
     else return 1.0 / factorial(n) + e(n - 1);
+}
+
+function bottomUp(n) {
+    if (n === 0)
+        return 1;
+
+    let fact = 1;
+    let e = 1;
+
+    for (let i = 1; i <= n; i++) {
+        fact *= i;
+        e += 1.0 / fact;
+    }
+
+    return e;
 }
